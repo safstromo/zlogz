4705b7024f8647f1c28c1c1c098933a944932940
Oliver Säfström <safstrom.oliver@gmail.com>
2024-06-05 14:27:52 +0200

    Create dir

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use simple_home_dir::home_dir;
+use std::fs;
+
+fn main() -> std::io::Result<()> {
+    let mut home_path = home_dir().expect("No homedir found");
+
+    home_path.push("zlogz");
+
+    println!("{:?}", &home_path);
+
+    match fs::create_dir(&home_path) {
+        Ok(_) => {
+            println!("zlogz directory created");
+        }
+        Err(e) if e.kind() == std::io::ErrorKind::AlreadyExists => {
+            println!("zlogz directory already exists");
+        }
+        Err(e) => {
+            println!("Failed to create zlogz directory: {}", e);
+            return Err(e);
+        }
+    }
+
+    Ok(())
 }
